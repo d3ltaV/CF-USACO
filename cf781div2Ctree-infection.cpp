40ab129f0490eb6e86e22dd5398229cc8aea2148
Joelle Yang <joellejingyaoyang@gmail.com>
2024-10-15 11:46:32 -0400

    prac

---
+++ cf781div2Ctree-infection.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <climits>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        vector<vector<int>> adj(n);
+
+    }
+}
